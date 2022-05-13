a1b7fbe49c8bbcb94892d058469f082e38fab17c
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-05-13 23:25:03 +0530

    Update __init__.py

--- meta_api/__init__.py
+++ meta_api/__init__.py
@@ -6,9 +6,10 @@ class META():
         self.url = "https://metavoid.info/api"
 
     def  animeimage(self, anime_name, type):
+        try:
             url = f"{self.url}/animeimage/{type}/{anime_name}"
             response = url
-            return response.json()
+            return response
         except  Exception as e:
             return "An error occured report on @metavoidsupport\n\n{}".format(e)
     
