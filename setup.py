1b9db4e56da964d25240175dca9e34f30c5ec46d
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-05-14 14:25:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as txt:
     long_description = txt.read()
 
 setuptools.setup(
-    name='meta_api',
+    name='metavoidapi',
     version='1.0.0',
-    description='meta_api Wrapper',
+    description='Metavoid Api Wrapper',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
     author='Tamilvip007',
     author_email='indrajeethy.it20@gmail.com',
-    url='https://github.com/TAMILVIP007/meta_api.git',
+    url='https://github.com/metavoidteam/meta_api.git',
     packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 4 - alpha',
