c8344873599cd363f6e2853d25e18a1f90dfc2cd
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-05-23 10:54:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as txt:
 
 setuptools.setup(
     name='metavoidapi',
-    version='1.0.4',
+    version='1.0.5',
     description='Metavoid Api Wrapper',
     long_description=long_description,
     long_description_content_type="text/markdown",
