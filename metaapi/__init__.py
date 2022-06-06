966c73d1aa7a7a252f3563ee0c86119cf739fbd3
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-06-06 07:30:16 +0530

    Update __init__.py

--- metaapi/__init__.py
+++ metaapi/__init__.py
@@ -125,6 +125,15 @@ class META():
         except Exception as e:
             return "An error occured report on @metavoidsupport\n\n{}".format(e)
 
+    def waifu(self):
+        try:
+            url = f"{self.url}/waifu"           
+            response = get(url, timeout=5)
+            return response.json()
+        except Exception as e:
+            return "An error occured report on @metavoidsupport\n\n{}".format(e)
+
+
 
     def torrent_1337x(self, text, page):
         try:
